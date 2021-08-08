6f2c5aa10f7e44d388a3312d6cf1260cfc81e815
Raj Patel <rajpatel131298@gmail.com>
2021-08-08 12:54:04 -0400

    Display live stock prices
    Ask user to enter the stock name and using yahoo finance inbuilt method display real-time stock price

--- main.py
+++ main.py
@@ -3,14 +3,26 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from yahoo_fin.stock_info import *
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# testing the yahoo finance methods
+# print(get_live_price("FB"))
+# print(get_quote_table("FB"))
+# print(get_live_price("AAPL"))
+# print(get_quote_table("AAPL"))
+
+# List of all S&P 500 companies
+print(tickers_sp500())
+
+# Getting input from user and displaying stock price
+stock = input("Please enter a ticket symbol:\n")
+
+if stock.upper() in tickers_sp500():
+    print("The stock is in the S&P 500.\n")
+    print("The stock price of "+stock.upper()+" is: ", str(round(get_live_price(stock.upper()), 2)))
+else:
+    print("Not in the S&P 500")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
